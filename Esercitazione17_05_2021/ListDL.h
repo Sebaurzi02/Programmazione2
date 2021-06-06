2a6793b313703b2e00de0b08b19dd2c73c2d2729
Sebaurzi02 <Sebaurzi02>
2021-06-06 13:22:01 +0200

    Implementazione classe struttura dati ListDL completa

--- Esercitazione17_05_2021/ListDL.h
+++ Esercitazione17_05_2021/ListDL.h
@@ -12,6 +12,36 @@ class ListDL{
 	     NodeDL<S> *head, *tail;
 	     int length;
 	     bool ascend;
+	     
+	    void insertAscending(S value){
+		   NodeDL<S> * temp = new NodeDL<S>(value);
+		   if(head == NULL && tail == NULL){ // in caso lista vuota
+			  this->insertHead(value);
+		      return;
+	    	}
+	    	
+		   if(head->getValue() > value) {
+			  this->insertHead(value);
+			  return;
+		    }
+		    
+		   if(tail->getValue() <= value){
+			  this->insertTail(value);
+			  return;
+		    }
+		    
+		   NodeDL<S> * current = head;
+		   //condizione arresto = nodo corrente minore di value e nodo successivo maggiore o uguale
+		   while(current->getValue() < value) { // arrivo tra i due nodi dove inserire quello nuovo
+			  current = current->getNext();
+		    }
+		
+		   temp->setPrev(current->getPrev());
+		   temp->setNext(current);
+		   current->getPrev()->setNext(temp);
+		   current->setPrev(temp);
+		   length++;
+	    }
 	
 	public:
 		ListDL() : ListDL(true) {}
@@ -41,7 +71,72 @@ class ListDL{
 		}
 		
 		void insertTail(S value){
+			NodeDL<S> * temp = new NodeDL<S>(value);
 			
+			if(head == NULL && tail == NULL){
+				head = temp;
+				tail = temp;
+			}
+			else{
+				temp->setPrev(tail);
+				tail->setNext(temp);
+				tail = temp;
+			}
+			
+			length++;
+		}
+		
+		void insert(S value){
+			if(this->isAscending()){
+				this->insertAscending(value);
+			}
+			else{
+				cout<<"to be implemented "<<endl;
+			}
+			return;
+		}
+		
+		void deleteValue(S value){
+			if(head == NULL && tail == NULL){
+				cout<< "Errore: empty list "<<endl;
+				return;
+			}
+			NodeDL<S> * cur = head;
+			while(cur != NULL && cur->getValue() != value){
+				cur = cur->getNext();
+			}
+			if(cur == NULL) {
+				cout<< "Errore: value "<< value << " not found." <<endl;
+				return;
+			}
+			if(cur != head) {
+				cur->getPrev()->setNext(cur->getNext());
+			}
+			if(cur != tail) {
+				cur->getNext()->setPrev(cur->getPrev());
+			}
+			
+			length--;
+			
+			if(cur == head) {
+				head = head->getNext();
+			}
+			
+			if(cur == tail) {
+				tail = tail->getPrev();
+			}
+			
+			delete cur;
+		}
+		
+		friend ostream& operator<<(ostream& out, const ListDL<S> list){
+			out << "Lista di lunghezza " <<list.length << ", head = "<< list.head << " ,tail = " << list.tail <<endl;
+			NodeDL<S> * cur = list.head;
+			while(cur != NULL){
+				out<< " \t " << *cur <<endl;
+				cur = cur->getNext();
+			}
+			return out;
 		}
 };
 #endif
